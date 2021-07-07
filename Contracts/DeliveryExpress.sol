c96b65354e2557df7227ea8ef0d1c6553f6c5132
Zakaria Hossain Foysal <zakaria15-9556@diu.edu.bd>
2021-07-07 14:35:25 +0600

    Update DeliveryExpress.sol

--- Contracts/DeliveryExpress.sol
+++ Contracts/DeliveryExpress.sol
@@ -1,60 +1,94 @@
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.6;
  
  
 contract DeliveryExpress {
+    
+    
    
     struct ShippingInfo {
         
+        uint del_id;
+        
         bool approval;
         uint cost;
         uint status;
         
-        
         address requestFrom;
         
-        bytes32 name;
-        bytes32 contactNo;
-        
-        uint truckLoadType; 
+        uint truckLoadType;
         uint distance;
         
-        bytes32 typeOfCommodity;
+        string typeOfCommodity;
     }
+    
+    
+    
+    
 
     struct CourierInfo {
         
+        uint del_id;
+        
         bool approval;
         uint cost;
         uint status;
         
         address requestFrom;
-        
-        bytes32 name;
-        bytes32 contactNo;
 
         uint height;
         uint width;
         uint depth;
         uint weight;
         
-        uint distance; 
+        uint distance;
         
-        bytes32 typeOfService;
+        string typeOfService;
     }
+    
+    
+    
 
     address public admin;
+    uint del_id = 1;
+    
+    
+    
+    address[] public users;
+
+
 
     mapping(address => CourierInfo[]) public courierUserInfo;
     mapping(address => ShippingInfo[]) public shippingUserInfo;
-
-    CourierInfo[] public couriers;
+    
+    
+    
     ShippingInfo[] public shippings;
+    CourierInfo[] public couriers;
+
+
+
 
 
      
     constructor() {
         admin = msg.sender;
     }
+    
+    
+    
+    function addUser(address sender) public{
+        uint f = 1;
+        for(uint i =0 ; i < users.length ; i ++)
+            if(sender == users[i])
+                f=0;
+        if(f==1)users.push(sender);
+    }
+    
+    
+    
+    
+    
+    
     
     function setApproval(bool approval, address userAddress, uint serviceType,uint ind) public{
         require(msg.sender == admin);
@@ -66,23 +100,101 @@ contract DeliveryExpress {
         }
     }
     
-    function addCourier(CourierInfo memory cInfo) public {
-        couriers.push(cInfo);
+    
+    
+    
+    
+    
+    
+    
+    function addCourier( uint cost , uint height , uint width , uint depth , uint weight , uint distance , string memory typeOfService) public {
+        
+        addUser(msg.sender);
+        
+        CourierInfo memory cInfo = CourierInfo({
+            cost : cost,
+            
+            height : height,
+            width : width,
+            depth : depth,
+            weight : weight,
+            
+            distance : distance,
+            
+            typeOfService : typeOfService,
+            
+            del_id : del_id++,
+            
+            approval : false,
+            status : 0,
+            requestFrom : msg.sender
+            
+        });
+        
         courierUserInfo[msg.sender].push(cInfo);
     }
     
-    function addShipping(ShippingInfo memory sInfo) public {
-        shippings.push(sInfo);
+    
+    function addShipping(uint cost,uint truckLoadType,uint distance,string memory typeOfCommodity) public {
+        
+        addUser(msg.sender);
+        
+        ShippingInfo memory sInfo = ShippingInfo({
+           cost : cost,
+           del_id : del_id++,
+           approval : false,
+           status: 0,
+           requestFrom : msg.sender,
+           
+           truckLoadType : truckLoadType,
+           distance : distance,
+           typeOfCommodity : typeOfCommodity
+        });
+        
         shippingUserInfo[msg.sender].push(sInfo);
+        
     }
     
-    function getAllShippings() public view returns(ShippingInfo[] memory){
+    
+    
+    
+    
+    
+    
+    function getAllShippings() public  returns(ShippingInfo[] memory){
+        
+        delete shippings;
+        
+        for(uint i = 0 ; i < users.length ; i ++){
+                for(uint j = 0 ; j < shippingUserInfo[users[i]].length ; j++){
+                    shippings.push(shippingUserInfo[users[i]][j]);
+            }
+        }
+        
+        
         return shippings;
     }
     
-    function getAllCouriers() public view returns(CourierInfo[] memory){
+    function getAllCouriers() public  returns(CourierInfo[] memory){
+        
+        delete couriers;
+        
+        for(uint i = 0 ; i < users.length ; i ++){
+                for(uint j = 0 ; j < courierUserInfo[users[i]].length ; j++){
+                    couriers.push(courierUserInfo[users[i]][j]);
+                }
+        }
+        
         return couriers;
     }
+    
+    
+    
+    
+    
+    
+    
+    
     
     function getShipping() public view returns(ShippingInfo[] memory){
         return shippingUserInfo[msg.sender];
